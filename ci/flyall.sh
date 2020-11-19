6cac360fbae18a593a7df803f2d554ce34f150d7
Jeff <11721615+jeffellin@users.noreply.github.com>
2020-11-19 14:04:30 -0500

    add httpbin clone controller for testing

--- ci/flyall.sh
+++ ci/flyall.sh
@@ -4,3 +4,4 @@ fly -t local sp -p vets-service -c spring-demo-pipeline.yml -v module=spring-pet
 fly -t local sp -p visits-service -c spring-demo-pipeline.yml -v module=spring-petclinic-visits-service
 fly -t local sp -p customer-service -c spring-demo-pipeline.yml -v module=spring-petclinic-customers-service
 fly -t local sp -p gateway-service -c spring-demo-pipeline.yml -v module=spring-petclinic-api-gateway
+fly -t local sp -p httpbin-service -c spring-demo-pipeline.yml -v module=spring-petclinic-httpbin
---
+++ spring-petclinic-httpbin/src/main/java/org/springframework/samples/petclinic/httpbin/web/HttpBinCompatibleController.java
@@ -0,0 +1,54 @@
+package org.springframework.samples.petclinic.httpbin.web;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.*;
+import java.io.*;
+import java.nio.charset.*;
+import java.util.*;
+@RestController
+public class HttpBinCompatibleController {
+
+    @GetMapping(path = "/get")
+    public Map get(HttpServletRequest request){
+        Map response = new HashMap();
+        Map headers = new HashMap<>();
+                for (String headerName : Collections.list(
+                        request.getHeaderNames())) {
+                    headers.put(headerName, request.getHeader(headerName));
+                }
+
+        response.put("url",getFullURL(request));
+        response.put("headers", headers);
+        response.put("parameters",mapParametersToJSON(request));
+
+        return response;
+    }
+
+    private String getFullURL(HttpServletRequest request) {
+        StringBuilder requestURL = new StringBuilder(
+            request.getRequestURL().toString());
+        String queryString = request.getQueryString();
+        if (queryString == null) {
+            return requestURL.toString();
+        } else {
+            return requestURL.append('?').append(queryString).toString();
+        }
+    }
+
+    private Map mapParametersToJSON(HttpServletRequest request) {
+        Map headers = new HashMap();
+
+        for (String name : Collections.list(request.getParameterNames())) {
+            String[] values = request.getParameterValues(name);
+            if (values.length == 1) {
+                headers.put(name, values[0]);
+            } else {
+                headers.put(name, values);
+            }
+        }
+
+        return headers;
+    }
+
+}

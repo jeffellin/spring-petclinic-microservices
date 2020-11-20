20e4b4a2841533880fdbd6b613cc26fc15df848a
Jeff <11721615+jeffellin@users.noreply.github.com>
2020-11-20 17:47:59 -0500

    Configure gateway for oauth

--- spring-petclinic-httpbin/src/main/java/org/springframework/samples/petclinic/httpbin/web/Security.java
+++ spring-petclinic-httpbin/src/main/java/org/springframework/samples/petclinic/httpbin/web/Security.java
@@ -1,53 +1,80 @@
 package org.springframework.samples.petclinic.httpbin.web;
 
-import org.keycloak.adapters.springboot.*;
-import org.keycloak.adapters.springsecurity.*;
-import org.keycloak.adapters.springsecurity.authentication.*;
-import org.keycloak.adapters.springsecurity.config.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.context.annotation.*;
-import org.springframework.security.config.annotation.authentication.builders.*;
-import org.springframework.security.config.annotation.web.builders.*;
-import org.springframework.security.config.annotation.web.configuration.*;
-import org.springframework.security.config.http.*;
-import org.springframework.security.core.authority.mapping.*;
-import org.springframework.security.core.session.*;
-import org.springframework.security.web.authentication.session.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @Configuration
 @EnableWebSecurity
-@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
-public class Security extends KeycloakWebSecurityConfigurerAdapter {
-    @Autowired
-    public void configureGlobal(
-        AuthenticationManagerBuilder auth) throws Exception {
-
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider
-            = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
-            new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
-    }
-
-    @Bean
-    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
-    }
-
-    @Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(
-            new SessionRegistryImpl());
-    }
+public class Security extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.authorizeRequests()
-            .antMatchers("/get*")
-            .hasRole("vets")
-            .anyRequest()
-            .permitAll();
+        // Validate tokens through configured OpenID Provider
+        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+        // Require authentication for all requests
+        http.authorizeRequests().anyRequest().authenticated();
+        // Allow showing pages within a frame
+        http.headers().frameOptions().sameOrigin();
     }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        // Convert realm_access.roles claims to granted authorities, for use in access decisions
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoderByIssuerUri(OAuth2ResourceServerProperties properties) {
+        String issuerUri = properties.getJwt().getIssuerUri();
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(issuerUri);
+        // Use preferred_username from claims as authentication name, instead of UUID subject
+        jwtDecoder.setClaimSetConverter(new UsernameSubClaimAdapter());
+        return jwtDecoder;
+    }
+
+}
+
+// As per: https://docs.spring.io/spring-security/site/docs/5.2.x/reference/html5/#oauth2resourceserver-jwt-claimsetmapping-rename
+class UsernameSubClaimAdapter implements Converter<Map<String, Object>, Map<String, Object>> {
+
+    private final MappedJwtClaimSetConverter delegate = MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
+
+    @Override
+    public Map<String, Object> convert(Map<String, Object> claims) {
+        Map<String, Object> convertedClaims = this.delegate.convert(claims);
+        String username = (String) convertedClaims.get("preferred_username");
+        convertedClaims.put("sub", username);
+        return convertedClaims;
+    }
+
+}
+
+class KeycloakRealmRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Collection<GrantedAuthority> convert(final Jwt jwt) {
+        final Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm_access");
+        return ((List<String>) realmAccess.get("roles")).stream()
+            .map(roleName -> "ROLE_" + roleName)
+            .map(SimpleGrantedAuthority::new)
+            .collect(Collectors.toList());
+    }
+
 }
